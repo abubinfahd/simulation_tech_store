83703805e3690f9d2b4733b5d2b56ff837ee928a
Abu Bin Fahd <192040@ku.ac.bd>
2021-04-13 00:38:03 +0600

    create a Tech Class

---
+++ tech_product.py
@@ -0,0 +1,21 @@
+class Tech:
+
+    total_products = 0
+    discount = 0.5
+
+    def __init__(self, name, price, weight, colour):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.colour = colour
+        Tech.total_products += 1
+
+    def apply_discount(self):
+        self.price = int(self.price - self.price * Tech.discount)
+
+    def calcuilate_shipping_cost(self, rate):
+        return f"Shipping cost will be{self.weight * rate}"
+
+    @classmethod
+    def get_total_products(cls):
+        return "Total products: " + str(cls.total_products)
